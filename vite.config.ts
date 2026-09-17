048eace5855b98a8e23cf62eebabafda8983e7e6
immlad <corniestc@proton.me>
2026-09-16 19:17:23 -0700

    Add tsconfig paths plugin to Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/lopsided/',   // REQUIRED for GitHub Pages
+  plugins: [react(), tsconfigPaths()],
+  base: '/lopsided/',
   build: {
     outDir: 'dist'
   }
